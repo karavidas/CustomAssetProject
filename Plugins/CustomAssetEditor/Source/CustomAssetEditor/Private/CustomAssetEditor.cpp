29cabcf6d9c5c871e9889f591ffcf55f8ac33b85
Lampros Karavidas <karavidaslampros@gmail.com>
2025-03-05 17:20:44 +0200

    Initial commit

---
+++ Plugins/CustomAssetEditor/Source/CustomAssetEditor/Private/CustomAssetEditor.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CustomAssetEditor.h"
+
+#define LOCTEXT_NAMESPACE "FCustomAssetEditorModule"
+
+void FCustomAssetEditorModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FCustomAssetEditorModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FCustomAssetEditorModule, CustomAssetEditor)
